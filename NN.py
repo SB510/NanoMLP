903d4e4c357dfae496bedf3aad3597fd0bf037b9
SB510 <sean.borneman@gmail.com>
2023-06-29 15:41:15 -0400

    begin to add training abstraction

---
+++ NN.py
@@ -0,0 +1,33 @@
+from Network import *
+
+
+
+class NN:
+    def __init__(self, model):
+        self.model = model
+    def train(self):    
+        x = [2.0, 3.0, -1.0]
+        n = MLP(3, [4, 4, 1])
+
+        xs = [
+        [2.0, 3.0, -1.0],
+        [3.0, -1.0, 0.5],
+        [0.5, 1.0, 1.0],
+        [1.0, 1.0, -1.0],
+        ]
+        ys = [1.0, -1.0, -1.0, 1.0] # desired targets
+        for k in range(200):
+            # forward pass
+            ypred = [n(x) for x in xs]
+            loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+            
+            # backward pass
+            for p in n.parameters():
+                p.grad = 0.0
+            loss.backward()
+            
+            # update
+            for p in n.parameters():
+                p.data += -0.01 * p.grad
+            
+            print(k, loss.data)
--- Network.py
+++ Network.py
@@ -58,19 +58,3 @@ class MLP(Module):
 
     def __repr__(self):
         return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
-    def draw(root):
-        dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
-        
-        nodes, edges = trace(root)
-        for n in nodes:
-            uid = str(id(n))
-            dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
-            if n._op:
-                dot.node(name = uid + n._op, label = n._op)
-                dot.edge(uid + n._op, uid)
-
-        for n1, n2 in edges:
-            # connect n1 to the op node of n2
-            dot.edge(str(id(n1)), str(id(n2)) + n2._op)
-
-        return dot
